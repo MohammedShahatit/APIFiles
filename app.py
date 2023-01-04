b5210ef6b5bc6649faa4ad6bbe11dffc9d673c4f
MohammedShahatit <121792931+MohammedShahatit@users.noreply.github.com>
2023-01-04 13:15:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask, render_template
+app =Flask(__name__)
 @app.route('/')
 def show_map():
-    return flask.render_template('map.html')
+    return render_template('map.html')
 if __name__ == '__main__':
     app.run(debug=True)
 
